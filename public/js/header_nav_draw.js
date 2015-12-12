275fa5187b2d1015ee781419394e4e5c0501dfcc
morninng <mr_morninng_star_dash@yahoo.co.jp>
2015-12-12 14:47:57 +0900

    external data is set when it cannnot be found

--- public/js/header_nav_draw.js
+++ public/js/header_nav_draw.js
@@ -3,12 +3,10 @@ window.onload = header_nav_draw;
 
  function RegistFbGraphData(){
    FB.api(
-   //   "/me?fields=picture,first_name,last_name,middle_name,timezone,gender,languages,link,religion,work,birthday,relationship_status,political,address,id,about,currency,devices,email,age_range,education",
    	"/me?fields=picture,first_name,last_name, email",
       function (response) {
         if (response && !response.error) {
 
-          console.log(response);
           var currentUser = Parse.User.current();
           var external_data_pointer = currentUser.get("ext_data");
 
@@ -21,6 +19,9 @@ window.onload = header_nav_draw;
           		},
           		error: function(){
           			console.log("ext data cannot be found");
+          			var User_Extension = Parse.Object.extend("User_Extension");
+          			var user_ext = new User_Extension();
+          			update_user_profile(response, currentUser, user_ext)
           		}
           	});
           }else{
@@ -37,31 +38,15 @@ window.onload = header_nav_draw;
  
 function update_user_profile(response, currentUser, user_ext){
 
-	// user_ext.set("timezone", response.timezone);
-	// user_ext.set("gender", response.gender);
-	// user_ext.set("languages", response.languages);
-	// user_ext.set("link", response.link);
-	// user_ext.set("religion", response.religion);
 
-	//  user_ext.set("work", response.work );  think how list can be saved
-	// user_ext.set("birthday", response.birthday );
-	// user_ext.set("relationship_status", response.relationship_status );
-	// user_ext.set("political", response.political );
-	// if(response.address){
-	//   user_ext.set("address", response.address.country );
-	// }
-	// user_ext.set("about", response.about );
 	user_ext.set("email", response.email );
-	// user_ext.set("age_range", response.age_range );
-	//   user_ext.set("education", response.education );  think how list can be saved
-	user_ext_ACL = new Parse.ACL(currentUser)
+	user_ext_ACL = new Parse.ACL(currentUser);
 	user_ext_ACL.setPublicReadAccess(true);
 	user_ext.setACL(user_ext_ACL);
 
 	currentUser.set("fb_id", response.id );
 	currentUser.set("FirstName", response.first_name);
 	currentUser.set("LastName", response.last_name);
-//	currentUser.set("MiddleName", response.middle_name);
 	currentUser.set("Profile_picture", response.picture.data.url);
 	currentUser.set("ext_data",user_ext)
 
